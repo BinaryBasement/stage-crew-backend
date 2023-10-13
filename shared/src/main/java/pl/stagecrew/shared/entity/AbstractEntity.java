e63f8bd9bcb05ee886fc6e668e9250cc060e8c1d
Adrian Wojtasik <wojtasik.a@outlook.com>
2023-10-13 14:18:13 +0200

    Refactor .gitignore

--- shared/src/main/java/pl/stagecrew/shared/entity/AbstractEntity.java
+++ shared/src/main/java/pl/stagecrew/shared/entity/AbstractEntity.java
@@ -2,7 +2,6 @@ package pl.stagecrew.shared.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
